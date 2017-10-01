a4a3f28fcb50dc20591a265da0b6c773aa7a0cbe
Anthony Massicci <anthony.massicci@outlook.com>
2017-10-01 16:11:04 -0400

    added function to build tree, made slight modifications to build_tokens.

--- html_compare.py
+++ html_compare.py
@@ -1,4 +1,7 @@
 import re
+from collections import namedtuple
+
+Node = namedtuple('Node', ['value', 'children'])
 
 def build_tokens(data):
 
@@ -11,4 +14,20 @@ def build_tokens(data):
         for match in re.finditer(regex, line):
             groups = match.groupdict()
             for k, v in groups.items():
-                if v and k != 'sp': yield (i + 1,k,v,)
+                if v: yield (i + 1,k,v.strip(),)
+
+def build_tree(tokens):
+    root = Node(None, [])     
+
+    def _build_tree(curr_node):
+        for token in tokens:
+            curr_node.children.append(Node(token, []))
+            if token[0] == 'etag': return
+            if token[0] == 'tag': _build_tree(curr_node.children[-1])
+
+    
+    _build_tree(root)
+    root.children.append(Node((0, 'eof', 'End of File'), []))
+    return root
+
+
